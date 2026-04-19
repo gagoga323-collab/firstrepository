d9b6b9a42b93414acb7b84e08f66277f8f852103
gagoga <gagoga323@gmail.com>
2026-04-19 17:18:17 +0800

    error_handler

--- tgbot.py
+++ tgbot.py
@@ -20,3 +20,109 @@ def send_welcome(message):
         'Введите название города на английском языке.'
     )
     bot.reply_to(message, welcome_text)
+
+@bot.message_handler(commands = ['weather'])
+def get_weather(message):
+    city = message.text.replace('/weather','').strip()
+
+    if not city:
+        bot.reply_to(
+            message,
+            'Пожалуйста, укажите название города\n'
+            'Пример: /weather Moscow'
+        )
+        return
+    
+    try:
+        params = {
+            'q': city,
+            'appid': api_key,
+            'units': 'metric',
+            'lang': 'ru'
+        }
+
+        response = requests.get(base_url, params = params)
+        data = response.json()
+
+        if response.status_code != 200:
+            if data.get('cod') == '404':
+                bot.reply_to(message, f'Город "{city}" не найден. Проверьте написание.')
+            else:
+                bot.reply_to(message, 'Ошибка при получении данных о погоде.')
+            return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# api_key = "cc2ba876736132e270670f8d7ebb0aef"
+# city = "Moscow,RU"
+# url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru"
+
+# response = requests.get(url)
+# data = response.json()
+
+# print(data)  
